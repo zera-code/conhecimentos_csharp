d9db7422691d8b3c75e521041123419b3fdb565a
Arthur Almeida <56892723+zera-code@users.noreply.github.com>
2022-08-30 15:39:54 -0300

    Add files via upload
    Arquivos do projeto 12 do curso c#: explorando a linguagem - alura

---
+++ P12-InvestimentoALongoPrazo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 12 - Investindo a longo prazo");
+
+        double fatorRendimento = 1.005;
+        double investimento = 1000;
+
+        for (int anos = 1; anos <= 5; anos++)
+        {
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                investimento *= fatorRendimento;
+            }
+            fatorRendimento += 0.001;
+        }
+
+        Console.WriteLine("Depois de 5 anos você terá R$ " + investimento);
+
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
