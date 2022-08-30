590886263534d357236588fb349b8f7801075351
Arthur Almeida <56892723+zera-code@users.noreply.github.com>
2022-08-30 15:35:17 -0300

    Add files via upload
    Arquivos do projeto 07 do curso c#: explorando a linguagem - alura

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executanto o projeto 7 - Condicionais");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 1;
+
+        if (idadeJoao >= 18)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+            if (quantidadePessoas > 1)
+            {
+                Console.WriteLine("Ele está acompanhado, pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar.");
+            }
+        }
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
