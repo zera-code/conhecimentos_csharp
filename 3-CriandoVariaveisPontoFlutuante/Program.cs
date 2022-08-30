3f3f19c9e597c24d9161dc0f0d15ac964997a8e8
Arthur Almeida <56892723+zera-code@users.noreply.github.com>
2022-08-30 15:32:26 -0300

    Add files via upload
    Arquivos do projeto 03 do curso c#: explorando a linguagem - alura

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+        double salario;
+        salario = 3000.13;
+
+        salario = 3000;
+
+        double idade = 7.0 / 5;
+
+        Console.WriteLine(idade);
+
+        Console.WriteLine(salario);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
