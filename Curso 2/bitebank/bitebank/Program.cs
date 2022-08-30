856447c5a3353fdfa9541db533f0c82d29af934b
Arthur Carnauba <arthur.carnauba@ferres.local>
2022-08-30 17:35:32 -0300

    Video sobre o valor padrão dos atributos de uma classe

--- Curso 2/bitebank/bitebank/Program.cs
+++ Curso 2/bitebank/bitebank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using bytebank;
 
+
 Console.WriteLine("Seja bem vindo ao seu banco: ByteBank!");
 
 
@@ -10,8 +11,32 @@ conta1.numero_agencia = 23;
 conta1.nome_agencia = "Agência Central";
 conta1.saldo = 100;
 
+
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = "Amanda Silva";
+conta2.conta = "111999-X";
+conta2.numero_agencia = 58;
+conta2.nome_agencia = "Ag^Çencia Central";
+conta2.saldo = 100;
+
+
+ContaCorrente conta3 = new ContaCorrente();
+
+
+
 Console.WriteLine("Titular: " + conta1.titular);
 Console.WriteLine("Conta: " + conta1.conta);
 Console.WriteLine("Número Agência: " + conta1.numero_agencia);
 Console.WriteLine("Nome da Agência: " + conta1.nome_agencia);
 Console.WriteLine("Saldo: " + conta1.saldo);
+Console.WriteLine("Verificador: " + conta1.verificador);
+
+
+Console.WriteLine("Titular: " + conta3.titular);
+Console.WriteLine("Conta: " + conta3.conta);
+Console.WriteLine("Número Agência: " + conta3.numero_agencia);
+Console.WriteLine("Nome da Agência: " + conta3.nome_agencia);
+Console.WriteLine("Saldo: " + conta3.saldo);
+Console.WriteLine("Verificador: " + conta3.verificador);
+
+
