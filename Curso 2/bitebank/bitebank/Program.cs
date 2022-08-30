cfdc8c3fdfcbf65e6683322bcbee2f4760945bd3
Arthur Carnauba <arthur.carnauba@ferres.local>
2022-08-30 18:18:33 -0300

    Arquivos do projeto do terceiro video da segunda aula - Tipo de referência

--- Curso 2/bitebank/bitebank/Program.cs
+++ Curso 2/bitebank/bitebank/Program.cs
@@ -5,34 +5,24 @@ Console.WriteLine("Seja bem vindo ao seu banco: ByteBank!");
 
 
 ContaCorrente conta1 = new ContaCorrente();
-conta1.titular = "Arthur Silva";
+conta1.titular = "Andre Silva";
 conta1.conta = "10123-X";
 conta1.numero_agencia = 23;
 conta1.nome_agencia = "Agência Central";
 conta1.saldo = 100;
 
-/*
+
 ContaCorrente conta2 = new ContaCorrente();
-conta2.titular = "Amanda Silva";
-conta2.conta = "111999-X";
-conta2.numero_agencia = 58;
+conta2.titular = "Andre Silva";
+conta2.conta = "10123-X";
+conta2.numero_agencia = 23;
 conta2.nome_agencia = "Agência Central";
 conta2.saldo = 100;
 
-
-Console.WriteLine("Titular: " + conta1.titular);
-Console.WriteLine("Conta: " + conta1.conta);
-Console.WriteLine("Número Agência: " + conta1.numero_agencia);
-Console.WriteLine("Nome da Agência: " + conta1.nome_agencia);
-Console.WriteLine("Saldo: " + conta1.saldo); */
-
-double saldo = 100;
-double saldo2 = conta1.saldo;
-conta1.saldo = 200;
+conta1 = conta2;
 
 
-Console.WriteLine(saldo == conta1.saldo);
-Console.WriteLine(saldo == saldo2);
+Console.WriteLine(conta1 == conta2);
 
 
 Console.ReadKey();
