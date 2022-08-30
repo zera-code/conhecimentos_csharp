1588320c6a05d69844cc83baa7e10945decc5e7c
Arthur Almeida <56892723+zera-code@users.noreply.github.com>
2022-08-30 15:36:46 -0300

    Add files via upload
    Arquivos do projeto 09 do curso c#: explorando a linguagem - alura

---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executanto o projeto 9 - Condicionais");
+
+        int idadeJoao = 16;
+        int quantidadePessoas = 2;
+
+        bool acompanhado = quantidadePessoas > 1;
+
+        string textoAdicional;
+
+
+        if (acompanhado == true)
+        {
+            textoAdicional = "João está acompanhado";
+        }
+        else
+        {
+            textoAdicional = "João não está acompanhado";
+        }
+
+        if (idadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar!");
+        }
+
+
+        //   if (idadeJoao >= 18 && quantidadePessoas > 1)
+        //   {
+        //       Console.WriteLine("Pode entrar!");
+        //   }
+        //   else
+        //   {
+        //       Console.WriteLine("Não pode entrar!");
+        //   }
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
