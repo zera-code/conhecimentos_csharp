b1ee14764dcd71bf623907cf6f52cf61fbd1f0a8
Arthur Carnauba <arthur.carnauba@ferres.local>
2022-09-01 16:13:14 -0300

    Arquivos do projeto do primeiro e segundo video da primeira aula - Apresentação/Organizando o projeto/Usando o this

---
+++ Curso 3/bytebank_ADM/bytebank_ADM/Funcionarios/Funcionario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Funcionarios
+{
+    public class Funcionario
+    {
+        // 0 - funcionário
+        // 1 - diretor
+        // 2 - designer
+        // N - ...
+
+        private int _tipo;
+
+        public Funcionario(int _tipo)
+        {
+            this._tipo = _tipo;
+        }
+
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public double Salario { get; set; }
+
+        public double getBonificacao()
+        {
+            if (_tipo == 1)
+            {
+                return Salario;
+            }
+            else
+            {
+                return Salario * 0.10;
+            }
+        }
+    }
+}
--- Curso 3/bytebank_ADM/bytebank_ADM/Program.cs
+++ Curso 3/bytebank_ADM/bytebank_ADM/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using bytebank_ADM.Funcionarios;
+
+Console.WriteLine("Boas Vundas ao ByteBank: Administração");
+
+Funcionario pedro = new Funcionario(1);
+pedro.Nome = "Pedro Miguel";
+pedro.Cpf = "12334566";
+pedro.Salario = 2000;
+
+Console.WriteLine("Nome: " + pedro.Nome);
+Console.WriteLine("CPF: " + pedro.Cpf);
+Console.WriteLine("Salário: " + pedro.Salario);
+
+Console.WriteLine("Bonificação: " + pedro.getBonificacao());
+Console.ReadKey();
+
